a3aedbaf8f1f248ca54d52849cc7629767ff8069
ayumu0106 <a.yamashita0106@gmail.com>
2025-04-17 13:56:28 +0900

    aaaaa

--- app/server.py
+++ app/server.py
@@ -1,17 +1,21 @@
-from threading import Thread
-
 from fastapi import FastAPI
 import uvicorn
+from threading import Thread  # Thread をインポート
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-	return {"message": "Server is Online."}
+    return {"message": "Server is Online."}
 
+# Uvicorn サーバーの起動関数
 def start():
-	uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
 
+# サーバーをバックグラウンドスレッドで起動する関数
 def server_thread():
-	t = Thread(target=start)
-	t.start()
+    t = Thread(target=start)
+    t.start()
+
+# サーバー起動
+server_thread()
